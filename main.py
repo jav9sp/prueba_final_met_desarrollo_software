f74d25c6de3f7f413af0efe1df7fcf590015c2d4
Francisco Fuentes <150530152+jav9sp@users.noreply.github.com>
2025-12-05 22:02:37 -0300

    Integracion multiplicacion y divisio. Actualizado el menu

--- main.py
+++ main.py
@@ -1,33 +1,41 @@
-while True:
-    print("\n=== CALCULADORA ===")
-    print("1. Sumar")
-    print("2. Restar")
-    print("3. Multiplicar")
-    print("4. Dividir")
-    print("5. Salir")
+from Calculadora import *
 
-    opcion = input("Elige una opción: ")
 
-    if opcion == "5":
-        print("Saliendo...")
-        break
+def menu():
+    while True:
+        print("\n=== CALCULADORA ===")
+        print("1. Sumar")
+        print("2. Restar")
+        print("3. Multiplicar")
+        print("4. Dividir")
+        print("5. Salir")
 
-    if opcion not in ["1", "2", "3", "4"]:
-        print("Opción inválida, intenta de nuevo.")
-        continue
+        opcion = input("Elige una opción: ")
 
-    try:
-        num1 = float(input("Ingresa el primer número: "))
-        num2 = float(input("Ingresa el segundo número: "))
-    except ValueError:
-        print("Error: debes ingresar números válidos.")
-        continue
+        if opcion == "5":
+            print("Saliendo...")
+            break
 
-    if opcion == "1":
-        pass
-    elif opcion == "2":
-        pass
-    elif opcion == "3":
-        pass
-    elif opcion == "4":
-        pass
+        if opcion not in ["1", "2", "3", "4"]:
+            print("Opción inválida, intenta de nuevo.")
+            continue
+
+        try:
+            num1 = float(input("Ingresa el primer número: "))
+            num2 = float(input("Ingresa el segundo número: "))
+        except ValueError:
+            print("Error: debes ingresar números válidos.")
+            continue
+
+        if opcion == "1":
+            print(f"El resultado es {sumar(num1, num2)}")
+        elif opcion == "2":
+            print(f"El resultado es {restar(num1, num2)}")
+        elif opcion == "3":
+            print(f"El resultado es {multiplicar(num1, num2)}")
+        elif opcion == "4":
+            print(f"El resultado es {dividir(num1, num2)}")
+
+
+if __name__ == "__main__":
+    menu()
